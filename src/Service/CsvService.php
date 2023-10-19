5b9ac1be692a8b18d46975e5ac71297da64f832b
Benoit Gueheneux <benoit.gueheneux@hotmail.com>
2023-10-19 11:02:26 +0200

    add-function-importcsv-dov

--- src/Service/CsvService.php
+++ src/Service/CsvService.php
@@ -4,6 +4,12 @@ namespace App\Service;
 
 class CsvService
 {
+    /**
+     * Imports data from a CSV file.
+     *
+     * @param $file A CSV file.
+     * @return array An array of email addresses extracted from the CSV file.
+     */
     public static function importCsv($file)
     {
         // Open the CSV file for reading
